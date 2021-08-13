ddc978ada017b9bda3981a22073f483d75948c83
Akim <52838912+akvise@users.noreply.github.com>
2021-08-13 16:15:27 +0300

    Revert "Homework7"

--- lesson_6/task_2/client.go
+++ lesson_6/task_2/client.go
@@ -8,21 +8,21 @@ import (
 )
 
 func main() {
-
 	conn, _ := net.Dial("tcp", "127.0.0.1:8080")
 	defer conn.Close()
-	fmt.Println("You can use 2 command:\n" +
-		"1. list\t\t- return list of users that store on the server\n" +
-		"2. register <name> - append new user with the name you indicated")
-
 	for {
-		fmt.Print("> ")
 		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Text to send: ")
 		text, _ := reader.ReadString('\n')
-
 		fmt.Fprintf(conn, text)
 		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message from server: " + message)
 
-		fmt.Print("server: " + message)
+		r := []rune(message)
+		r = r[:len(r)-2]
+		text = string(r)
+		if text == "EXIT" {
+			break
+		}
 	}
 }
